470f87a7407313a37c6745844c3c0fb4c494ab1f
Leonardo Vilca <leovilca63@gmail.com>
2023-04-01 06:06:00 -0300

    Agregando lab02.

---
+++ lab02/ej0/fixstring.c
@@ -0,0 +1,26 @@
+#include <stdbool.h>
+#include <assert.h>
+
+#include "fixstring.h"
+
+unsigned int fstring_length(fixstring s) {
+    /*
+     * COMPLETAR
+     *
+     */
+}
+
+bool fstring_eq(fixstring s1, fixstring s2) {
+    /*
+     * COMPLETAR
+     *
+     */
+}
+
+bool fstring_less_eq(fixstring s1, fixstring s2) {
+    /*
+     * COMPLETAR
+     *
+     */
+}
+
