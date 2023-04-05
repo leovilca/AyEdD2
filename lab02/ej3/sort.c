30c33f630ff4ace300437bd3e4ca6d4d62817cf2
Leonardo Vilca <leovilca63@gmail.com>
2023-04-05 09:53:18 -0300

    Lab02 - Ej3 - Prueba

--- lab02/ej3/sort.c
+++ lab02/ej3/sort.c
@@ -7,6 +7,19 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
+ /* needs implementation */
+
+    if(der>izq){
+        unsigned int ppiv = partition(a,izq,der);
+        if(ppiv>izq){
+            quick_sort_rec(a,izq,ppiv-1);
+        }
+        if(ppiv<der){
+            quick_sort_rec(a,ppiv+1,der);
+        }
+    }
+}
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
     /* Needs implementation */
@@ -16,6 +29,21 @@ static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
      - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
      - a[pivot] 'goes_before' all the elements in a(pivot,der]
     */
+
+    unsigned int ppiv = izq;
+    int i=izq+1,j=der;
+    while(i<=j){
+        if(a[i]<=a[ppiv]){
+            i++;
+        }else if(a[j]>=a[ppiv]){
+            j--;
+        }else{
+            swap(a,i,j);
+        }
+    }
+    swap(a,ppiv,i);
+    ppiv=i;
+    return ppiv;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
