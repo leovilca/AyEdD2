e9cb1987969f07c0e5e4af6fbf557ab18763770e
Leonardo Vilca <leovilca63@gmail.com>
2023-04-05 05:34:40 -0300

    lab02 - Ej1 - prueba

--- lab02/ej1/sort.c
+++ lab02/ej1/sort.c
@@ -9,11 +9,18 @@
 
 static void insert(int a[], unsigned int i) {
     /* needs implementation */
+    if(i>1){
+        unsigned int j=i-1;
+        while(j>0u && goes_before(a[i],a[j])){
+            j--;
+        }
+        swap(a,i,j);
+    }
 }
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1u; i < length; ++i) {
-        /* needs implementation */
+        assert(array_is_sorted(a,i-1));
         insert(a, i);
     }
 }
